e846ac76274f93e676df6646b4c1bc61c4f77f42
robert-dunnagan <166849562+robert-dunnagan@users.noreply.github.com>
2024-05-09 11:04:16 -0400

    Move Object methods to module scope

--- include/nodel/pyext/module.h
+++ include/nodel/pyext/module.h
@@ -4,5 +4,6 @@ extern "C" {
 
 extern PyModuleDef nodel_module_def;
 extern PyObject* nodel_sentinel;
+extern PyObject* iter_keys(PyObject* arg);
 
 } // extern C
--- pyext/module.cpp
+++ pyext/module.cpp
@@ -29,6 +29,43 @@ PyObject* nodel_sentinel = nullptr;
 static python::Support support;
 
 //-----------------------------------------------------------------------------
+// Utility functions
+//-----------------------------------------------------------------------------
+
+inline
+NodelObject* wrap(PyObject* mod, const Object& obj) {
+    NodelObject* nd_obj = (NodelObject*)PyObject_CallMethodObjArgs(mod, PyUnicode_InternFromString("Object"), NULL);
+    if (nd_obj != NULL) nd_obj->obj = obj;
+    return nd_obj;
+}
+
+inline
+NodelObject* as_nodel_object(PyObject* arg) {
+    return NodelObject_CheckExact(arg)? (NodelObject*)arg: NULL;
+}
+
+PyObject* iter_keys(PyObject* arg) {
+    NodelObject* nd_self = as_nodel_object(arg);
+    if (nd_self == NULL) return NULL;
+
+    NodelKeyIter* nit = (NodelKeyIter*)NodelKeyIterType.tp_alloc(&NodelKeyIterType, 0);
+    if (nit == NULL) return NULL;
+    if (NodelKeyIterType.tp_init((PyObject*)nit, NULL, NULL) == -1) return NULL;
+
+    try {
+        nit->range = nd_self->obj.iter_keys();
+        nit->it = nit->range.begin();
+        nit->end = nit->range.end();
+
+        Py_INCREF(nit);
+        return (PyObject*)nit;
+    } catch (const NodelException& ex) {
+        PyErr_SetString(PyExc_RuntimeError, ex.what());
+        return NULL;
+    }
+}
+
+//-----------------------------------------------------------------------------
 // Module methods
 //-----------------------------------------------------------------------------
 
@@ -39,7 +76,7 @@ constexpr auto mod_bind_doc = "Bind object with URI string/dict\n"
 static PyObject* mod_bind(PyObject* mod, PyObject* arg) {
     try {
         URI uri{support.to_object(arg)};
-        return (PyObject*)NodelObject_wrap(bind(uri));
+        return (PyObject*)wrap(mod, bind(uri));
     } catch (const NodelException& ex) {
         PyErr_SetString(PyExc_RuntimeError, ex.what());
         return NULL;
@@ -71,9 +108,254 @@ static PyObject* mod_from_json(PyObject* mod, PyObject* arg) {
     return po.get_clear();
 }
 
+constexpr auto root_method_doc =
+"Returns the root of the tree containing the argument.\n"
+"nodel.root(obj) -> Object\n"
+"Returns the root of the tree, which may be the argument.";
+
+static PyObject* mod_root(PyObject* mod, PyObject* arg) {
+    NodelObject* nd_self = as_nodel_object(arg);
+    if (nd_self == NULL) return NULL;
+    return (PyObject*)wrap(mod, nd_self->obj.root());
+}
+
+constexpr auto parent_method_doc =
+"Returns the parent of the argument.\n"
+"nodel.parent(obj) -> Object\n"
+"If the argument is the root of the tree, then nil is returned.\n"
+"Returns the parent of the argument, which may be nil.";
+
+static PyObject* mod_parent(PyObject* mod, PyObject* arg) {
+    NodelObject* nd_self = as_nodel_object(arg);
+    if (nd_self == NULL) return NULL;
+    return (PyObject*)wrap(mod, nd_self->obj.parent());
+}
+
+constexpr auto iter_keys_method_doc =
+"Returns an iterator over the keys of the argument.\n"
+"nodel.iter_keys(obj) -> iterator\n"
+"If the argument is any kind of map, then this method returns an iterator over\n"
+"keys in the map.\n"
+"If the argument is a list, then this method returns an iterator over the\n"
+"indices of the list - similar to the `enumerate` builtin function.\n"
+"Otherwise, a RuntimeError exception is raised.\n"
+"Returns an iterator over the keys of the argument.";
+
+static PyObject* mod_iter_keys(PyObject* mod, PyObject* arg) {
+    return iter_keys(arg);
+}
+
+constexpr auto iter_values_method_doc =
+"Returns an iterator over the values (Object instances) of the argument.\n"
+"nodel.iter_values(obj) -> iterator\n"
+"If the argument is any kind of map, then this method returns an iterator over\n"
+"values in the map.\n"
+"If the argument is a list, then this method returns an iterator over the\n"
+"elements in the list.\n"
+"Otherwise, a RuntimeError exception is raised.\n"
+"Returns an iterator over the values of the argument.";
+
+static PyObject* mod_iter_values(PyObject* mod, PyObject* arg) {
+    NodelObject* nd_self = as_nodel_object(arg);
+    if (nd_self == NULL) return NULL;
+
+    NodelValueIter* nit = (NodelValueIter*)NodelValueIterType.tp_alloc(&NodelValueIterType, 0);
+    if (nit == NULL) return NULL;
+    if (NodelValueIterType.tp_init((PyObject*)nit, NULL, NULL) == -1) return NULL;
+
+    try {
+        nit->range = nd_self->obj.iter_values();
+        nit->it = nit->range.begin();
+        nit->end = nit->range.end();
+
+        Py_INCREF(nit);
+        return (PyObject*)nit;
+    } catch (const NodelException& ex) {
+        PyErr_SetString(PyExc_RuntimeError, ex.what());
+        return NULL;
+    }
+}
+
+constexpr auto iter_items_method_doc =
+"Returns an iterator over the items of the argument.\n"
+"nodel.iter_items(obj) -> iterator\n"
+"If the argument is any kind of map, then this method returns an iterator over\n"
+"key/value pairs in the map.\n"
+"If the argument is a list, then this method returns an iterator over the\n"
+"index/value pairs in the list, similar to the `enumerate` builtin function.\n"
+"Otherwise, a RuntimeError exception is raised.\n"
+"Returns an iterator over the items of the argument.";
+
+static PyObject* mod_iter_items(PyObject* mod, PyObject* arg) {
+    NodelObject* nd_self = as_nodel_object(arg);
+    if (nd_self == NULL) return NULL;
+
+    NodelItemIter* nit = (NodelItemIter*)NodelItemIterType.tp_alloc(&NodelItemIterType, 0);
+    if (nit == NULL) return NULL;
+    if (NodelItemIterType.tp_init((PyObject*)nit, NULL, NULL) == -1) return NULL;
+
+    try {
+        nit->range = nd_self->obj.iter_items();
+        nit->it = nit->range.begin();
+        nit->end = nit->range.end();
+
+        Py_INCREF(nit);
+        return (PyObject*)nit;
+    } catch (const NodelException& ex) {
+        PyErr_SetString(PyExc_RuntimeError, ex.what());
+        return NULL;
+    }
+}
+
+constexpr auto iter_tree_method_doc =
+"Returns an iterator of the sub-tree whose root is the argument.\n"
+"nodel.iter_tree(obj) -> iterator\n"
+"A tree iterator iterates over the values (instances of Object) throughout\n"
+"the entire tree whose root is this object. Be careful using this method\n"
+"since it can result in loading every bound object in the sub-tree.\n"
+"This method traverses both map and list containers - for example, a list\n"
+"whose elements are lists or maps.\n"
+"Returns an iterator over every Object in the sub-tree.";
+
+static PyObject* mod_iter_tree(PyObject* mod, PyObject* arg) {
+    NodelObject* nd_self = as_nodel_object(arg);
+    if (nd_self == NULL) return NULL;
+
+    NodelTreeIter* nit = (NodelTreeIter*)NodelTreeIterType.tp_alloc(&NodelTreeIterType, 0);
+    if (nit == NULL) return NULL;
+    if (NodelTreeIterType.tp_init((PyObject*)nit, NULL, NULL) == -1) return NULL;
+
+    try {
+        nit->range = nd_self->obj.iter_tree();
+        nit->it = nit->range.begin();
+        nit->end = nit->range.end();
+
+        Py_INCREF(nit);
+        return (PyObject*)nit;
+    } catch (const NodelException& ex) {
+        PyErr_SetString(PyExc_RuntimeError, ex.what());
+        return NULL;
+    }
+}
+
+constexpr auto key_method_doc =
+"Returns the key/index of the argument within its parent.\n"
+"nodel.key(obj) -> any\n"
+"Returns nil or the key/index of the argument.";
+
+static PyObject* mod_key(PyObject* mod, PyObject* arg) {
+    NodelObject* nd_self = as_nodel_object(arg);
+    if (nd_self == NULL) return NULL;
+
+    try {
+        return support.to_py(nd_self->obj.key());
+    } catch (const NodelException& ex) {
+        PyErr_SetString(PyExc_RuntimeError, ex.what());
+        return NULL;
+    }
+}
+
+constexpr auto reset_method_doc = \
+"Reset a bound object, clearing it and releasing memory resources.\n"
+"nodel.reset(obj) -> None\n"
+"If the argument is bound (has data-source), then it's cleared and the data-source\n"
+"will re-load on the next data access. Any references to descendants will be\n"
+"orphaned, and cannot be re-loaded, themselves.";
+
+static PyObject* mod_reset(PyObject* mod, PyObject* arg) {
+    NodelObject* nd_self = as_nodel_object(arg);
+    if (nd_self == NULL) return NULL;
+
+    try {
+        nd_self->obj.reset();
+        Py_RETURN_NONE;
+    } catch (const NodelException& ex) {
+        PyErr_SetString(PyExc_RuntimeError, ex.what());
+        return NULL;
+    }
+}
+
+constexpr auto reset_key_method_doc = \
+"Reset the key of a sparse DataSource, clearing it and releasing memory resources.\n"
+"nodel.reset_key(obj, key) -> None\n"
+"If the object is bound (has data-source), then the key is cleared and the data-source\n"
+"will re-load the key on the next access. Any references to descendants of the value\n"
+"of the key will be orphaned, and cannot be re-loaded, themselves.";
+
+static PyObject* mod_reset_key(PyObject* self, PyObject* const* args, Py_ssize_t nargs) {
+    if (nargs != 2) {
+        PyErr_SetString(PyExc_ValueError, "Wrong number of arguments");
+        return NULL;
+    }
+
+    NodelObject* nd_self = as_nodel_object(args[0]);
+    if (nd_self == NULL) return NULL;
+
+    try {
+        nd_self->obj.reset_key(support.to_key(args[1]));
+        Py_RETURN_NONE;
+    } catch (const NodelException& ex) {
+        PyErr_SetString(PyExc_RuntimeError, ex.what());
+        return NULL;
+    }
+}
+
+constexpr auto save_method_doc = \
+"Commit all updates to bound objects in the subtree whose root is the argument.\n"
+"nodel.save(obj) -> None\n"
+"Objects that are not bound (do not have a data-source) are ignored.";
+
+static PyObject* mod_save(PyObject* mod, PyObject* arg) {
+    NodelObject* nd_self = as_nodel_object(arg);
+    if (nd_self == NULL) return NULL;
+
+    try {
+        nd_self->obj.save();
+        Py_RETURN_NONE;
+    } catch (const NodelException& ex) {
+        PyErr_SetString(PyExc_RuntimeError, ex.what());
+        return NULL;
+    }
+}
+
+constexpr auto is_same_method_doc =
+"True, if the first and second arguments are the same Object.\n"
+"nodel.is_same(l_obj, r_obj) -> bool\n"
+"Since the backing C++ class, Object, is a reference, the Python keyword, 'is', will return\n"
+"False even if two instances of the C++ Object class point to the same data.\n Therefore,\n"
+"this function should always be used to test that two Nodel Objects are the same Object.\n"
+"Returns True if the two Nodel Object instances point to the same data";
+
+static PyObject* mod_is_same(PyObject* mod, PyObject* const* args, Py_ssize_t nargs) {
+    if (nargs != 2) {
+        PyErr_SetString(PyExc_ValueError, "Wrong number of arguments");
+        return NULL;
+    }
+
+    NodelObject* lhs = as_nodel_object(args[0]);
+    if (lhs == NULL) return NULL;
+
+    NodelObject* rhs = as_nodel_object(args[1]);
+    if (rhs == NULL) return NULL;
+
+    if (lhs->obj.is(rhs->obj)) Py_RETURN_TRUE; else Py_RETURN_FALSE;
+}
+
+
 static PyMethodDef nodel_methods[] = {
-    {"bind",      (PyCFunction)mod_bind,      METH_O, PyDoc_STR(mod_bind_doc)},
-    {"from_json", (PyCFunction)mod_from_json, METH_O, PyDoc_STR(mod_from_json_doc)},
+    {"bind",        (PyCFunction)mod_bind,                METH_O,        PyDoc_STR(mod_bind_doc)},
+    {"from_json",   (PyCFunction)mod_from_json,           METH_O,        PyDoc_STR(mod_from_json_doc)},
+    {"root",        (PyCFunction)mod_root,                METH_O,        PyDoc_STR(root_method_doc)},
+    {"parent",      (PyCFunction)mod_parent,              METH_O,        PyDoc_STR(parent_method_doc)},
+    {"iter_keys",   (PyCFunction)mod_iter_keys,           METH_O,        PyDoc_STR(iter_keys_method_doc)},
+    {"iter_values", (PyCFunction)mod_iter_values,         METH_O,        PyDoc_STR(iter_values_method_doc)},
+    {"iter_items",  (PyCFunction)mod_iter_items,          METH_O,        PyDoc_STR(iter_items_method_doc)},
+    {"iter_tree",   (PyCFunction)mod_iter_tree,           METH_O,        PyDoc_STR(iter_tree_method_doc)},
+    {"key",         (PyCFunction)mod_key,                 METH_O,        PyDoc_STR(key_method_doc)},
+    {"reset",       (PyCFunction)mod_reset,               METH_O,        PyDoc_STR(reset_method_doc)},
+    {"reset_key",   (PyCFunction)mod_reset_key,           METH_FASTCALL, PyDoc_STR(reset_key_method_doc)},
+    {"save",        (PyCFunction)mod_save,                METH_O,        PyDoc_STR(save_method_doc)},
+    {"is_same",     (PyCFunction)mod_is_same,             METH_FASTCALL, PyDoc_STR(is_same_method_doc)},
     {NULL, NULL, 0, NULL}
 };
 
