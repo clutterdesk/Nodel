cdbc6fd9e64c1c4d7fb4e9649b8356585e10197e
robert-dunnagan <166849562+robert-dunnagan@users.noreply.github.com>
2024-05-28 08:47:26 -0400

    Rework of dependencies in cmake

--- pyext/setup.py
+++ pyext/setup.py
@@ -1,6 +1,7 @@
 from setuptools import Extension, setup
 import os
 import re
+import glob
 
 nodel_wflags = re.split(r'\s+', os.environ.get('NODEL_WFLAGS'))
 nodel_pyext_include = re.split(r'\s+', os.environ.get('NODEL_PYEXT_INCLUDE'))
@@ -9,15 +10,13 @@ nodel_pyext_lib_path = re.split(r'\s+', os.environ.get('NODEL_PYEXT_LIB_PATH'))
 print('~' * 100)
 print(os.getcwd())
 print('~' * 100)
-print(nodel_pyext_include);
-print('~' * 100)
-print(nodel_pyext_lib_path);
-print('~' * 100)
 
 setup(
     ext_modules=[
         Extension(
             name='nodel',
+            version='0.1',
+            url='https://github.com',
             define_macros=[
                 ('PYNODEL_ROCKSDB', 1)
             ],
@@ -35,16 +34,19 @@ setup(
             extra_compile_args=[
                 '--std=c++20',
                 '-Wno-c99-designator',
+                '-Wno-delete-non-abstract-non-virtual-dtor',
                 *nodel_wflags
             ],
             library_dirs=[
                 *nodel_pyext_lib_path
             ],
             libraries=[
-                'fmt',
                 'dwarf',
+                'zstd',
                 'cpptrace',
-                'speedb'
+                'fmt',
+                'speedb',
+                'ziplib'
             ],
             extra_link_args=[]
         ),
