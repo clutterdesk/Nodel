d42d87897fcc4ad3f2cc07b2ac688ccd2e0b8e35
robert-dunnagan <166849562+robert-dunnagan@users.noreply.github.com>
2024-05-30 12:12:03 -0400

    Still working on cmake build (sigh)

--- pyext/setup.py
+++ pyext/setup.py
@@ -11,12 +11,60 @@ print('~' * 100)
 print(os.getcwd())
 print('~' * 100)
 
+
+# lookup <module-path>/..
+mod_path = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+
+def find_includes(dirs):
+    paths = []
+    for dir in dirs:
+        path = next((entry[0] for entry in os.walk(mod_path) 
+                     if dir in os.path.join(entry[0], entry[1])[-len(dir):]), None)
+        if path is None:
+            sys.stderr.write(f'Include file not found: {dir}')
+            sys.exit(-1)
+        paths.append()
+    return paths
+
+def find_libs(names):
+    paths = []
+    for name in names:
+        path = next((entry[0] for entry in os.walk(mod_path) if name in entry[2]), None)
+        if path is None:
+            sys.stderr.write(f'Library not found: {name}')
+            sys.exit(-1)
+        paths.append()
+    return paths
+
+
+includes = [
+    'Source/ZipLib',
+    'include/cpptrace',
+    'include/fmt',
+    'include/tsl',
+    'include/rocksdb'
+]
+
+libs = [
+    'dwarf',
+    'zstd',
+    'cpptrace',
+    'fmt',
+    'speedb',
+    'ziplib'
+]
+
+
+include_paths = find_includes(includes)
+lib_paths = find_libs([f'lib{lib}.a' for lib in libs])
+
+
 setup(
     ext_modules=[
         Extension(
             name='nodel',
             version='0.1',
-            url='https://github.com',
+            url='https://github.com/clutterdesk/Nodel',
             define_macros=[
                 ('PYNODEL_ROCKSDB', 1)
             ],
@@ -28,26 +76,15 @@ setup(
                 'NodelItemIter.cxx',
                 'NodelTreeIter.cxx'
             ],
-            include_dirs=[
-                *nodel_pyext_include
-            ],
+            include_dirs=include_paths,
             extra_compile_args=[
                 '--std=c++20',
                 '-Wno-c99-designator',
                 '-Wno-delete-non-abstract-non-virtual-dtor',
                 *nodel_wflags
             ],
-            library_dirs=[
-                *nodel_pyext_lib_path
-            ],
-            libraries=[
-                'dwarf',
-                'zstd',
-                'cpptrace',
-                'fmt',
-                'speedb',
-                'ziplib'
-            ],
+            library_dirs=lib_paths,
+            libraries=libs,
             extra_link_args=[]
         ),
     ]
