eeb4505e2c360c27dd344be75fe0957f919cbf4c
robert-dunnagan <166849562+robert-dunnagan@users.noreply.github.com>
2025-04-28 12:18:25 -0400

    ...

--- pyext/test/test_module.py
+++ pyext/test/test_module.py
@@ -1,245 +1,106 @@
-import unittest
 import math
+import os
 import sys
+import unittest
+import urllib.parse
 import nodel as nd
 
 import logging 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 
 
-class TestObject(unittest.TestCase):
-    def test_ctor(self):
-        examples = [None, True, 7, 3.14, 'tea', [1, 2, 3]]
-        for example in examples:
-            obj = nd.Object(example)
-            self.assertEqual(obj, example)
-            self.assertEqual(str(obj), str(example))
-
-        example = {'tea': 'Assam'}
-        obj = nd.Object(example)
-        self.assertEqual(obj, example)
-        self.assertEqual(str(obj), '{"tea": "Assam"}')
+class TestModule(unittest.TestCase):
+    def test_bind(self):
+        path = urllib.parse.quote(os.path.dirname(__file__))
+        wd = nd.bind(f'file://?path={path}')
+        self.assertTrue('test_module.py' in wd)
         
-    def test_nil(self):
-        obj = nd.Object(None)
-        self.assertTrue(nd.is_nil(obj))
-        self.assertEqual(str(obj), "None")
+    def test_url_path_conflict(self):
+        self.assertRaises(RuntimeError, nd.bind, f'file:///?path=.')  
 
-    def test_nil_to_native(self):
-        obj = nd.Object(None)
-        pyo = nd.native(obj)
-        self.assertIs(pyo, None)
+    def test_from_json(self):
+        d = nd.from_json('{"drink": "tea", "optional_drink": null, "cups": 2, "grams": 3.5}')
+        self.assertEqual(d.drink, 'tea')
+        self.assertIsNone(d.optional_drink)
+        self.assertEqual(d.cups, 2)
+        self.assertEqual(d.grams, 3.5)
         
-    def test_int(self):
-        obj = nd.Object(1)
-        self.assertTrue(nd.is_int(obj))
-        self.assertEqual(str(obj), "1")
-        obj = nd.Object(-1)
-        self.assertTrue(nd.is_int(obj))
-        self.assertEqual(str(obj), "-1")
+    def test_clear_map(self):
+        d = nd.from_json('{"drink": "tea"}')
+        nd.clear(d)
+        self.assertEqual(len(d), 0)
+
+    def test_clear_list(self):
+        l = nd.Object(['tea', 'tisane'])
+        nd.clear(l)
+        self.assertEqual(len(l), 0)
     
-    def test_int_to_native(self):
-        obj = nd.Object(1)
-        pyo = nd.native(obj)
-        self.assertIs(type(pyo), int)
-        
-    def test_max_mach_int(self):
-        obj = nd.Object(sys.maxsize)
-        self.assertTrue(nd.is_int(obj))
-        self.assertEqual(str(obj), str(sys.maxsize))
-        obj = nd.Object(-sys.maxsize)
-        self.assertTrue(nd.is_int(obj))
-        self.assertEqual(str(obj), str(-sys.maxsize))
+    def test_clear_string(self):
+        s = nd.Object('tea')
+        nd.clear(s)
+        self.assertEqual(s, '')
 
-    def test_int_arith(self):
-        x, y = nd.Object(7), nd.Object(3)
-        # avoid testing richcompare, here
-        self.assertEqual(x + y, 10)
-        self.assertEqual(x - y, 4)
-        self.assertEqual(x * y, 21)
-        self.assertEqual(x / y, nd.native(x) / nd.native(y))
-        self.assertEqual(x // y, 2)
-        self.assertEqual(x % y, 1)
-        q, m = divmod(x, y)
-        self.assertEqual(q, 2)
-        self.assertEqual(m, 1)
-        self.assertEqual(-x, -7)
-        self.assertEqual(+x, 7)
-        self.assertEqual(x ** y, nd.native(x) ** nd.native(y))
-        self.assertEqual(pow(x, 3), pow(nd.native(x), 3))
-        self.assertEqual(pow(7, y), pow(7, nd.native(y)))
-        self.assertEqual(pow(x, y), pow(nd.native(x), nd.native(y)))
-        self.assertEqual(pow(x, y, 2), pow(nd.native(x), nd.native(y), 2))
-        self.assertEqual(pow(x, y, nd.Object(2)), pow(nd.native(x), nd.native(y), 2))
-
-    def test_int_logic(self):
-        x, y = nd.Object(7), nd.Object(3)
-        # avoid testing richcompare, here
-        self.assertEqual(x & y, 3)
-        self.assertEqual(x | y, 7)
-        self.assertEqual(x ^ y, 4)
-        self.assertEqual(~x, -8)
-        
-    def test_int_inplace_arith(self):
-        x, y = nd.Object(7), nd.Object(3)
-        # avoid testing richcompare, here
-        x += y
-        self.assertTrue(y, 3)
-        self.assertTrue(nd.is_int(x))
-        self.assertEqual(x, 10)
-        x = nd.Object(7)
-        x -= y
-        self.assertTrue(y, 3)
-        self.assertTrue(nd.is_int(x))
-        self.assertEqual(x, 4)
-        x = nd.Object(7)
-        x *= y
-        self.assertTrue(y, 3)
-        self.assertTrue(nd.is_int(x))
-        self.assertEqual(x, 21)
-        x = nd.Object(7)
-        x /= y
-        self.assertTrue(y, 3)
-        self.assertTrue(nd.is_float(x))
-        self.assertEqual(x, 7 / 3)
-        x = nd.Object(7)
-        x //= y
-        self.assertTrue(y, 3)
-        self.assertTrue(nd.is_int(x))
-        self.assertEqual(x, 2)
-        x = nd.Object(7)
-        x %= y
-        self.assertTrue(y, 3)
-        self.assertTrue(nd.is_int(x))
-        self.assertEqual(x, 1)
-        x = nd.Object(7)
-        x **= y
-        self.assertTrue(y, 3)
-        self.assertTrue(nd.is_int(x))
-        self.assertEqual(x, 343)
-        
-    def test_int_inplace_logic(self):
-        x, y = nd.Object(7), nd.Object(3)
-        # avoid testing richcompare, here
-        x &= y
-        self.assertEqual(x, 3)
-        x = nd.Object(7)
-        x |= y
-        self.assertEqual(x, 7)
-        x = nd.Object(7)
-        x ^= y
-        self.assertEqual(x, 4)
-        x = nd.Object(7)
-        
-    def test_index_conversion(self):
-        self.assertEqual([1, 'bing!', 3][nd.Object(1)], 'bing!')
-        
-    def test_float(self):
-        obj = nd.Object(math.pi)
-        self.assertTrue(nd.is_float(obj))
-        self.assertEqual(str(obj), str(math.pi))
+    def test_clear_wrong_type(self):
+        self.assertRaises(RuntimeError, nd.clear, nd.Object(5))
     
-    def test_float_to_native(self):
-        obj = nd.Object(math.e)
-        pyo = nd.native(obj)
-        self.assertIs(type(pyo), float)
+    def test_root(self):
+        d = nd.from_json("{'x': {'y': {'z': 'yep, tea'}}}")
+        self.assertEqual(nd.root(d.x.y.z).x.y.z, 'yep, tea')
         
-    def test_string_basic(self):
-        obj = nd.Object("tea")
-        self.assertTrue(nd.is_str(obj))
-        self.assertEqual(str(obj), "tea")
-        self.assertEqual(repr(obj), '"tea"')
-    
-    def test_string_with_nulls(self):
-        obj = nd.Object("tea\0time")
-        self.assertTrue(nd.is_str(obj))
-        self.assertEqual(str(obj), "tea\x00time")
-        self.assertEqual(repr(obj), '"tea\x00time"')
+    def test_parent(self):
+        d = nd.from_json("{'x': [{'u': 'oops'}, {'u': 'doh'}]}")
+        self.assertTrue(nd.is_same(nd.parent(d.x), d))
+        self.assertTrue(nd.is_same(nd.parent(d.x[1]), d.x))
+        self.assertTrue(nd.is_same(nd.parent(d.x[1].u), d.x[1]))
         
-    @unittest.expectedFailure
-    def test_string_slice(self):
-        obj = nd.Object("teatime")
-        self.assertEqual(obj[:3], "tea")
-        
-    def test_string_to_native(self):
-        obj = nd.Object("teatime")
-        s = nd.native(obj)
-        self.assertIsInstance(s, str)
-        self.assertEqual(s[:3], "tea")
+    def test_is_same(self):
+        d = nd.from_json("{'drink': 'tea'}")
+        self.assertTrue(nd.is_same(nd.root(d.drink), d))
+        self.assertIsNot(nd.root(d.drink), d)  # NOTE this
 
-    def test_float_arith(self):
-        x, y = nd.Object(7.0), nd.Object(3.0)
-        # avoid testing richcompare, here
-        self.assertEqual(x + y, 10.0)
-        self.assertEqual(x - y, 4.0)
-        self.assertEqual(x * y, 21.0)
-        self.assertEqual(x / y, nd.native(x) / nd.native(y))
-        self.assertEqual(x // y, 2.0)
-        self.assertEqual(x % y, 1.0)
-        q, m = divmod(x, y)
-        self.assertEqual(q, 2.0)
-        self.assertEqual(m, 1.0)
-        self.assertEqual(x ** y, nd.native(x) ** nd.native(y))
-        self.assertEqual(-x, -7.0)
-        self.assertEqual(+x, 7.0)
+    def test_list_iter_keys(self):
+        l = nd.from_json("[1, 2]")
+        self.assertEqual(list(nd.iter_keys(l)), [0, 1])
+        
+    def test_list_iter_values(self):
+        l = nd.from_json("[1, 2]")
+        self.assertEqual(list(nd.iter_values(l)), [1, 2])
+        
+    def test_list_iter_items(self):
+        l = nd.from_json("[1, 2]")
+        self.assertEqual(list(nd.iter_items(l)), [(0, 1), (1, 2)])
 
-    def test_list_len(self):
-        self.assertEqual(len(nd.Object([1, 2, 3])), 3)
-            
-    def test_list_subscript(self):
-        list = nd.Object([1, 2, 3])
-        self.assertEqual((list[0], list[1], list[2]), (1, 2, 3))
-        self.assertEqual(list[:2], [1, 2])
-        self.assertEqual(list[:-1], [1, 2])
-        self.assertEqual(list[1:], [2, 3])
-        self.assertEqual(list[-1:], [3])
-        self.assertEqual(list[::2], [1, 3])
-        self.assertEqual(list[:], [1, 2, 3])
-        self.assertEqual(list[::], [1, 2, 3])
-        self.assertEqual(list[-1::-1], [3, 2, 1])
-        self.assertEqual(list[-1:-1:-1], [])
+    def test_map_iter_keys(self):
+        d = nd.from_json("{'x': 1, 'y': 2}")
+        self.assertEqual(list(nd.iter_keys(d)), ['x', 'y'])
         
-    def test_list_iter(self):
-        list = nd.Object([1, 2, 3])
-        it = iter(list)
-        self.assertEqual(next(it), 1)
-        self.assertEqual(next(it), 2)
-        self.assertEqual(next(it), 3)
+    def test_map_iter_values(self):
+        d = nd.from_json("{'x': 1, 'y': 2}")
+        self.assertEqual(list(nd.iter_values(d)), [1, 2])
+        
+    def test_map_iter_items(self):
+        d = nd.from_json("{'x': 1, 'y': 2}")
+        self.assertEqual(list(nd.iter_items(d)), [('x', 1), ('y', 2)])
 
-    def test_list_with_opaque(self):
-        class X:
-            pass
-        x = X()
-        list = nd.Object([x, x])
-        self.assertIs(nd.native(list[0]), x)
-        self.assertIs(nd.native(list[1]), x)
-
-    def test_map_len(self):
-        self.assertEqual(len(nd.Object({'x': 1, 'y': 2, 'z': 3})), 3)
+    def test_map_iter_tree(self):
+        d = nd.from_json("{'x': [{'u': 1}, {'u': 2}], 'y': [{'u': 3}, {'u': 4}]}")
+        expect = [d, d.x, d.y, d.x[0], d.x[1], d.y[0], d.y[1], d.x[0].u, d.x[1].u, d.y[0].u, d.y[1].u]
+        for actual, result in zip(nd.iter_tree(d), expect):
+            self.assertTrue(nd.is_same(actual, result))
         
-    def test_map_subscript(self):
-        map = nd.Object({'x': 1, 'y': 2, 'z': 3})
-        self.assertEqual(map['x'], 1)
-        self.assertEqual(map['y'], 2)
-        self.assertEqual(map['z'], 3)
+    def test_key(self):
+        d = nd.from_json("{'x': [1, 2, 3]}")
+        self.assertEqual(nd.key(d.x), 'x')
         
-    @unittest.expectedFailure
-    def test_map_object_subscript(self):
-        self.assertEqual(map[nd.Object('z')], 3)
+    def test_reset(self):
+        path = urllib.parse.quote(os.path.dirname(__file__))
+        wd = nd.bind(f'file://?path={path}&perm=rw')
+        self.assertTrue('test_module.py' in wd)
+        nd.clear(wd)
+        self.assertTrue('test_module.py' not in wd)
+        nd.reset(wd)
+        self.assertTrue('test_module.py' in wd)
         
-    def test_map_attr_subscript(self):
-        map = nd.Object({'x': 1, 'y': 2, 'z': 3})
-        self.assertEqual(map.x, 1)
-        self.assertEqual(map.y, 2)
-        self.assertEqual(map.z, 3)
-        
-    def test_map_with_opaque(self):
-        class X:
-            pass
-        x = X()
-        map = nd.Object({'x': x})
-        self.assertIs(nd.native(map.x), x)
-
         
 if __name__ == '__main__':
     unittest.main()
